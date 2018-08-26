167b7ba5639f537ae9c28d261a465c0930721882
shayekh00 <shayekhnavid@gmail.com>
2018-08-26 20:40:20 +0600

    Learning Python

---
+++ demo.py
@@ -0,0 +1,13 @@
+from sklearn import tree
+
+X=[ [181,80,44],[177,70,43],[181,80,44],[181,80,44],[181,80,44],[181,80,44],[181,80,44],[181,80,44],[181,80,44],[181,80,44],[181,80,44]]
+
+Y= ['male','female','female','male' ,'female','male','female','female','male' ,'female','male']
+
+clf=tree.DecisionTreeClassifier()
+
+clf=clf.fit(X,Y)
+
+prediction=clf.predict([[190,70,42]])
+
+print (prediction)
